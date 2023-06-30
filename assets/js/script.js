4b659298fab38b14050bfa1baa1bf0112e797208
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-06-30 23:59:55 +0000

    chore: add timer countdown functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,71 @@ export const lightbulb = document.querySelector(".lightbulb")
 export const play = document.querySelector(".play")
 export const stopwatch = document.querySelector(".stopwatch")
 export const volume = document.querySelector(".volume")
+const timer = document.querySelector(".timer")
+
+let minutes, seconds, countdown
 
 lightbulb.onclick = () => {
   Button.turnOffLight()
 }
 
 play.onclick = () => {
-  Button.play()
+  if (!play.classList.contains("pause")) {
+    Button.play()
+    timerCountdown()
+  } else {
+    Button.pause()
+    clearInterval(countdown)
+  }
 }
 
 stopwatch.onclick = () => {
-  Button.stopwatch()
+  if (!stopwatch.classList.contains("stop")) {
+    Button.stopwatch()
+  } else {
+    Button.stop()
+  }
 }
 
 volume.onclick = () => {
-  Button.volume()
+  if (!volume.classList.contains("volume")) {
+    Button.volume()
+  } else {
+    Button.mute()
+  }
+}
+
+const updateTimerDisplay = (minutes, seconds) => {
+  let minutesOnDisplay = document.querySelector(".minutes")
+  let secondsOnDisplay = document.querySelector(".seconds")
+
+  minutesOnDisplay.innerHTML = String(minutes).padStart(2, "0")
+  secondsOnDisplay.innerHTML = String(seconds).padStart(2, "0")
+}
+
+const timerCountdown = () => {
+  minutes = 10
+  seconds = 60
+
+  countdown = setInterval(() => {
+    if (seconds <= 0) {
+      minutes--
+      seconds = 59
+    } else {
+      seconds--
+    }
+
+    updateTimerDisplay(minutes, seconds)
+
+    if (minutes === 0 && seconds <= 10) {
+      timer.classList.add("txt-red")
+    }
+
+    if (minutes === 0 && seconds === 0) {
+      clearInterval(countdown)
+      Button.play()
+      Button.stopwatch()
+      timer.classList.remove("txt-red")
+    }
+  }, 1000)
 }
