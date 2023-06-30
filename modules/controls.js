3e3c9f48b657e89b83926843c09228506d66ab5d
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-06-30 23:58:59 +0000

    chore: add event click to each button separately

--- modules/controls.js
+++ modules/controls.js
@@ -8,14 +8,34 @@ import {
 
 const Button = {
   play() {
-    play.classList.toggle("pause")
+    play.classList.add("pause")
+    play.classList.remove("play")
     stopwatch.classList.add("stop")
+    stopwatch.classList.remove("stopwatch")
+  },
+  pause() {
+    play.classList.remove("pause")
+    play.classList.add("play")
+    stopwatch.classList.add("stopwatch")
+    stopwatch.classList.remove("stop")
+  },
+  stop() {
+    stopwatch.classList.remove("stop")
+    stopwatch.classList.add("stopwatch")
+    play.classList.remove("pause")
+    play.classList.add("play")
   },
   stopwatch() {
-    stopwatch.classList.toggle("stop")
+    stopwatch.classList.add("stopwatch")
+    stopwatch.classList.remove("stop")
   },
   volume() {
-    volume.classList.toggle("mute")
+    volume.classList.add("volume")
+    volume.classList.remove("mute")
+  },
+  mute() {
+    volume.classList.add("mute")
+    volume.classList.remove("volume")
   },
   turnOffLight() {
     body.style.opacity =
